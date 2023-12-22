98345354a0d6fec8bcc7167eda8828354f1e016b
AdrienBlanco <adrienblanco89@gmail.com>
2023-12-22 12:19:38 +0100

    Export des données dans un fichier json

--- src/components/ProjectsList/index.jsx
+++ src/components/ProjectsList/index.jsx
@@ -1,60 +1,11 @@
 import React, { useEffect, useRef } from "react";
 import "./style.scss";
 import ProjectCard from "../ProjectCard";
-import ArgentBank from "../../img/ArgentBank.webp";
-import Booki from "../../img/Booki.webp";
-import OhMyFood from "../../img/OhMyFood.webp";
-import Kasa from "../../img/Kasa.webp";
-import Events from "../../img/events.webp";
-import logo from "../../img/logo2.png";
-// import projectsData from "../../../public/data/projectsData.json";
+import projectsData from "../../data/projectsData.json";
 import gsap from "gsap";
 import {ScrollTrigger} from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
-
-const projectArr = [
-    {
-        img: ArgentBank,
-        title: "Argent Bank",
-        title_des: "Implémenter le front-end d'une application bancaire avec React",
-        description: "Développement du front-end d'une nouvelle banque en ligne avec React & Redux. Gestion des connexions utilisateurs et affichages de leurs comptes et transactions ainsi que la possiblité de modifier leur pseudo. Le tout via des appels API. Création de nouvelles routes API pour la gestion des transactions.",
-        skills: "Javascript | React | Sass | Redux | Swagger",                 
-        github: "https://github.com/AdrienBlanco/OCProjet11"
-    },
-    {
-        img: Booki,
-        title: "Booki",
-        title_des: "Créer la page d'accueil d'une agence de voyage avec HTML & CSS",
-        description: "Création de la page d'accueil d'une agence de voyage. L’entreprise souhaite développer un site Internet qui permette aux usagers de trouver des hébergements et des activités dans la ville de leur choix. Site entièrement réalisé en Html et Css.",
-        skills: "HTML | CSS",                      
-        github: "https://github.com/AdrienBlanco/OCProjet3"
-    },    
-    {
-        img: OhMyFood,
-        title: "Ohmyfood",
-        title_des: "Améliorer l'interface d'un site mobile avec des animations CSS",
-        description: "Développement d'un site en mobile first qui répertorie les menus de restaurants gastronomiques. Style et animations réalisés avec Sass.",
-        skills: "HTML | Sass",                 
-        github: "https://github.com/AdrienBlanco/OCProjet4"
-    },    
-    {
-        img: Kasa,
-        title: "Kasa",
-        title_des: "Créer une application web de location immobilière avec React",
-        description: "Refonte d'un site de location d'appartements. Intégration de React avec des composants et les différentes routes. Création d'animations CSS de menus déroulants. Mise en place de requêtes vers un fichier Json pour alimenter les différents contenus sur le site. Intégration du site de manière responsive.",
-        skills: "Javascript | React | Sass",                 
-        github: "https://github.com/AdrienBlanco/OCProjet8"
-    },    
-    {
-        img: Events,
-        title: "724events",
-        title_des: "Débugger le site d'une agence d'événementiel",
-        description: "Débogage d'un site implémenté avec React, en utilisant des tests unitaires et d'intégrations réalisés avec Jest. Ecriture d'un cahier de recette des différentes fonctionnalités du site.",
-        skills: "Javascript | React | Sass | Jest",                 
-        github: "https://github.com/AdrienBlanco/OCProjet10"
-    },
-]
 
 export default function Projects(){
 
@@ -87,9 +38,9 @@ export default function Projects(){
     },[])
 
     return(
-        <section className="project-content">
+        <section className="project-content" id="project">
             <div className="intro">
-                <img src={logo} alt="symbole circulaire" className="logo" ref={imgRef} />
+                <img src="./img/logo2.png" alt="symbole circulaire" className="logo" ref={imgRef} />
                 <div className="text">
                     <h2 ref={textH1Ref}>PROJETS</h2>
                     <p ref={infoRef}>
@@ -100,7 +51,7 @@ export default function Projects(){
                 </div>
             </div>
             <div className="projects" ref={projRef}>
-                {projectArr.map((item, index)=>(
+                {projectsData.map((item, index)=>(
                     <ProjectCard 
                         key={index} 
                         img={item.img}
